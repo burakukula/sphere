7272e7adf1db304c28baf28579e8713f7c067c79
patrycjaradaczynska <patrycja.radaczynska@brainly.com>
2017-04-23 10:59:13 +0200

    Adding interactions

--- js/ex1.js
+++ js/ex1.js
@@ -21,10 +21,7 @@ scene.add(light);
 var loadingManager = new THREE.LoadingManager();
 var loader = new THREE.TextureLoader(loadingManager);
 
-
-
 var texture = loader.load("img/earth_colored2.png", myInit);
-console.log(texture);
 
 var earthGeo = new THREE.SphereGeometry (30, 40, 400);
 var earthMat = new THREE.MeshPhongMaterial({
@@ -35,7 +32,6 @@ var earthMat = new THREE.MeshPhongMaterial({
 var earthMesh = new THREE.Mesh(earthGeo, earthMat);
 earthMesh.position.set(-100, 0, 0);
 
-
 scene.add(earthMesh);
 camera.lookAt( earthMesh.position );
 
@@ -44,12 +40,60 @@ renderer.setSize(WIDTH, HEIGHT);
 renderer.domElement.style.position = 'relative';
 
 container.appendChild(renderer.domElement);
+
 function myInit(texture) {
   render();
 }
+
+var isDragging = false;
+var previousMousePosition = {
+    x: 0,
+    y: 0
+};
+
+const toRadians = (angle) => {
+    return angle * (Math.PI / 180);
+};
+
+const toDegrees = (angle) => {
+    return angle * (180 / Math.PI);
+};
+
+const renderArea = renderer.domElement;
+
+renderArea.addEventListener('mousedown', (e) => {
+    isDragging = true;
+});
+
+renderArea.addEventListener('mousemove', (e) => {
+    var deltaMove = {
+        x: e.offsetX-previousMousePosition.x,
+        y: e.offsetY-previousMousePosition.y
+    };
+
+    if(isDragging) {
+
+        let deltaRotationQuaternion = new THREE.Quaternion().
+        setFromEuler(
+            new THREE.Euler(toRadians(deltaMove.y * 1), toRadians(deltaMove.x * 1), 0, 'XYZ')
+        );
+
+        earthMesh.quaternion.multiplyQuaternions(deltaRotationQuaternion, earthMesh.quaternion);
+    }
+
+    previousMousePosition = {
+        x: e.offsetX,
+        y: e.offsetY
+    };
+});
+
+document.addEventListener('mouseup', (e) => {
+    isDragging = false;
+});
+
 function render () {
-  // requestAnimationFrame( render );
-  // earthMesh.rotation.y += 0.001;
+  requestAnimationFrame( render );
+  earthMesh.rotation.y += 0.001;
 
   renderer.render(scene, camera);
 }
