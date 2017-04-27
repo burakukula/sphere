17ae1e7084fea245a605c96f96d9bb52b92b001c
patrycjaradaczynska <patrycja.radaczynska@brainly.com>
2017-04-27 15:53:04 +0200

    Caluculations into px

--- js/ex2.js
+++ js/ex2.js
@@ -1,10 +1,20 @@
-const $face = document.querySelector('.face');
+const $face = document.querySelector('.js-f1');
+const $face2 = document.querySelector('.js-f2');
+const $face3 = document.querySelector('.js-f3');
 const $container = document.querySelector('.container');
 const setCanvas = document.getElementById('canvas');
 const radius = 20;
 const width = 600;
 const height = 600;
 
+const cameraZposition = 150;
+const cameraDeg = 45;
+const cameraDegRad = (2 * Math.PI) * cameraDeg/360;
+
+const sceneWidth = (2 * cameraZposition) * Math.tan(cameraDegRad/2);
+const units = width/sceneWidth;
+
+$container.style.perspective = 100 * units + 'px';
 
 // renderer
 const renderer = new THREE.WebGLRenderer({antialias : true, canvas : setCanvas});
@@ -20,8 +30,9 @@ const light = new THREE.HemisphereLight( 0xdbdbdb, 0x1b2e57, 2 );
 scene.add( light );
 
 // camera
-const camera = new THREE.PerspectiveCamera( 45, width / height, 1, 100 );
-camera.position.set(0, 0, 100);
+const camera = new THREE.PerspectiveCamera( cameraDeg, width / height, 1, 200 );
+// const camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 5, 1000);
+camera.position.set(0, 0, cameraZposition);
 
 // texture loader
 const loadingManager = new THREE.LoadingManager();
@@ -29,7 +40,7 @@ const loader = new THREE.TextureLoader(loadingManager);
 
 // texture
 const texture = loader.load('img/map-4-100.jpg');
-const sphereGeo = new THREE.SphereGeometry (radius, 20, 200);
+const sphereGeo = new THREE.SphereGeometry (radius, 200, 200);
 const sphereMat = new THREE.MeshPhongMaterial({
   color: 0xaaaaaa,
   map: texture
@@ -43,12 +54,33 @@ camera.lookAt( sphere.position );
 
 // object
 const obj = new THREE.Object3D();
+const obj2 = new THREE.Object3D();
+const obj3 = new THREE.Object3D();
 obj.position.z = radius;
+obj2.position.y = 10;
+obj2.position.z = 17.320;
+obj3.position.y = -10;
+obj3.position.z = 17.320;
 sphere.add( obj );
+sphere.add( obj2 );
+sphere.add( obj3 );
+
+const points = [
+  {
+    obj: obj,
+    el: $face
+  }, {
+    obj: obj2,
+    el: $face2
+  }, {
+    obj: obj3,
+    el: $face3
+  }
+]
 
 // update matrix
 sphere.updateMatrixWorld();
-const position = new THREE.Vector3();
+// const position = new THREE.Vector3();
 
 // update element position
 const updatePosition = (el, x, y, z) => {
@@ -59,11 +91,12 @@ const animate = () => {
 	requestAnimationFrame( animate );
   sphere.rotation.y += 0.01;
 
-  position.setFromMatrixPosition(obj.matrixWorld);
-  xParam = position.x * (width/111);
-  updatePosition($face, xParam , position.y, position.z);
-
-  position.z < 0 ? $face.classList.add('hidden') : $face.classList.remove('hidden');
+  points.forEach(({el, obj}) => {
+    let pos = new THREE.Vector3();
+    pos.setFromMatrixPosition(obj.matrixWorld);
+    updatePosition(el, pos.x * units, pos.y * units, pos.z * units);
+    pos.z < 0 ? el.classList.add('hidden') : el.classList.remove('hidden');
+  });
 
 	renderer.render( scene, camera );
 
